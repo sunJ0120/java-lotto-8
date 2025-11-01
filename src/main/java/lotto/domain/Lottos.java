19db03411e909d1f379e9cc4bcc1912e0d62ae17
sunJ0120 <sspure1214@gmail.com>
2025-11-01 14:48:12 +0900

    feat: Lotto 객체를 모아둔 Lottos 객체 생성
    Lottos는 단순히 모아두기 위한 것으로, 등수 정보 같은건 모른채로 오직 Lotto 집합만 나타내도록 설정

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    private Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos generate(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+        return new Lottos(lottos);
+    }
+
+    public List<Lotto> getLottos() {
+        return List.copyOf(lottos);
+    }
+
+    public int size() {
+        return lottos.size();
+    }
+}
