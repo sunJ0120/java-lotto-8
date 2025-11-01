3466dbfb3438f63a5adc8d143d777d5781066967
sunJ0120 <sspure1214@gmail.com>
2025-11-01 17:02:29 +0900

    refactor: 큰 기능을 나눠서 Service로 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,37 +1,20 @@
 package lotto.controller;
 
-import java.util.List;
-import lotto.domain.Lotto;
-import lotto.domain.LottoResult;
 import lotto.domain.Lottos;
-import lotto.util.InputParser;
-import lotto.view.InputView;
-import lotto.view.OutputView;
+import lotto.service.CompareLottoService;
+import lotto.service.PurchaseLottoService;
 
 public class LottoController {
-    public void run(){
-        InputView inputView = new InputView();
-        InputParser inputParser = new InputParser();
-        int purchaseAmount = inputView.readPurchaseAmount();
-        System.out.println();
-        int purchaseCount = purchaseAmount/1000;
+    private PurchaseLottoService purchaseLottoService;
+    private CompareLottoService compareLottoService;
 
-        Lottos lottos = Lottos.generate(purchaseCount);
-        OutputView outputView = new OutputView();
-        outputView.printPurchaseCount(lottos);
-        outputView.printLottos(lottos);
-        System.out.println();
+    public LottoController(PurchaseLottoService purchaseLottoService, CompareLottoService compareLottoService) {
+        this.purchaseLottoService = purchaseLottoService;
+        this.compareLottoService = compareLottoService;
+    }
 
-        String winningNumber = inputView.readWinningNumbers();
-        List<Integer> winningNumbers = inputParser.parseWinningNumbers(winningNumber);
-        System.out.println();
-
-        Lotto winningLotto = new Lotto(winningNumbers);
-        int bonusNumber = inputView.readBonusNumber();
-        System.out.println();
-
-        LottoResult lottoResult = new LottoResult(winningLotto, bonusNumber);
-        outputView.printPrizeStatistics(lottoResult, lottos);
-        System.out.println();
+    public void run() {
+        Lottos lottos = purchaseLottoService.buyLottos();
+        compareLottoService.compareAndPrintResult(lottos);
     }
 }
