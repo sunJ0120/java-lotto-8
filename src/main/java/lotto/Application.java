feecf04dfb26f1e27d2f897feb2cc14e82ff190b
sunJ0120 <sspure1214@gmail.com>
2025-11-01 17:02:59 +0900

    refactor: service 분리에 따른 로직 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,22 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.service.CompareLottoService;
+import lotto.service.PurchaseLottoService;
+import lotto.util.InputParser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        LottoController controller = new LottoController();
+        InputView inputView = new InputView();
+        InputParser inputParser = new InputParser();
+        OutputView outputView = new OutputView();
+
+        PurchaseLottoService purchaseLottoService = new PurchaseLottoService(inputView, outputView);
+        CompareLottoService compareLottoService = new CompareLottoService(inputView, inputParser, outputView);
+
+        LottoController controller = new LottoController(purchaseLottoService, compareLottoService);
         controller.run();
     }
 }
