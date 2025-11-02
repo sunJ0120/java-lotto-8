256b217b4ebd69a5fcedb8c98ebbfc9be2150509
sunJ0120 <sspure1214@gmail.com>
2025-11-02 17:41:38 +0900

    feat: 사용자가 입력한 당첨 번호와 보너스 번호를 가지는 객체 생성

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,13 +1,38 @@
 package lotto.domain;
 
-import lotto.Lotto;
-
 public class WinningNumbers {
-    private Lotto winningLotto;
-    private int bonusNumber;
+    private final Lotto winningLotto;
+    private final int bonusNumber;
 
     public WinningNumbers(Lotto winningLotto, int bonusNumber) {
+        validate(winningLotto, bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private static void validateBonusNumberDuplicate(Lotto winningLotto, int bonusNumber) {
+        if (winningLotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private static void validateBonusNumberRange(int bonusNumber) {
+        if (1 > bonusNumber || bonusNumber > 45) {
+            throw new IllegalArgumentException(
+                    String.format("[ERROR] 로또 번호는 1 ~ 45까지의 범위만 허용합니다. : %d", bonusNumber)
+            );
+        }
+    }
+
+    public void validate(Lotto winningLotto, int bonusNumber) {
+        validateBonusNumberRange(bonusNumber);
+        validateBonusNumberDuplicate(winningLotto, bonusNumber);
+    }
+
+    public Rank calculateRank(Lotto purchasedLotto) {
+        int matchLottoCount = winningLotto.countMatches(purchasedLotto);
+        boolean hasBonusNumber = purchasedLotto.contains(bonusNumber);
+
+        return Rank.valueOf(matchLottoCount, hasBonusNumber);
     }
 }
