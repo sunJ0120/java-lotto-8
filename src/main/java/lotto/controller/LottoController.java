23b8824b4d1b0e1f742993adeec0eaa42b90e8b6
sunJ0120 <sspure1214@gmail.com>
2025-11-02 23:36:08 +0900

    refactor: Controller에서 view와 소통 & 흐름 관리만 하도록 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,20 +1,55 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.LottoResult;
 import lotto.domain.Lottos;
-import lotto.service.CompareLottoService;
-import lotto.service.PurchaseLottoService;
+import lotto.domain.WinningNumbers;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
-    private PurchaseLottoService purchaseLottoService;
-    private CompareLottoService compareLottoService;
+    private final InputView inputView;
+    private final LottoService lottoService;
+    private final OutputView outputView;
 
-    public LottoController(PurchaseLottoService purchaseLottoService, CompareLottoService compareLottoService) {
-        this.purchaseLottoService = purchaseLottoService;
-        this.compareLottoService = compareLottoService;
+    public LottoController(InputView inputView, LottoService lottoService, OutputView outputView) {
+        this.inputView = inputView;
+        this.lottoService = lottoService;
+        this.outputView = outputView;
     }
 
     public void run() {
-        Lottos lottos = purchaseLottoService.buyLottos();
-        compareLottoService.compareAndPrintResult(lottos);
+        Lottos lottos = purchaseLottos();
+        WinningNumbers winningNumbers = receiveWinningNumbers();
+        calculateAndPrintResult(winningNumbers, lottos);
+    }
+
+    private void calculateAndPrintResult(WinningNumbers winningNumbers, Lottos lottos) {
+        LottoResult lottoResult = lottoService.createLottoResult(winningNumbers);
+        outputView.printPrizeStatistics(lottoResult, lottos);
+        System.out.println();
+    }
+
+    private WinningNumbers receiveWinningNumbers() {
+        List<Integer> parseWinningNumbers = inputView.readWinningNumbers();
+        System.out.println();
+
+        int bonusNumber = inputView.readBonusNumber();
+        System.out.println();
+
+        return lottoService.createWinningNumbers(parseWinningNumbers, bonusNumber);
+    }
+
+    private Lottos purchaseLottos() {
+        int purchaseAmount = inputView.readPurchaseAmount();
+        System.out.println();
+
+        Lottos lottos = lottoService.purchaseLottos(purchaseAmount);
+        outputView.printPurchaseCount(lottos);
+        outputView.printLottos(lottos);
+        System.out.println();
+
+        return lottos;
     }
 }
