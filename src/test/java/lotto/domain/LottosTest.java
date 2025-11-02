41436760fb43f0daad0f1797af77b34927944a26
sunJ0120 <sspure1214@gmail.com>
2025-11-02 17:18:46 +0900

    test: Lottos test 작성 완료
    1. 들어온 count 만큼의 Lotto가 생성되는지 검증
    2. getLottos가 불변 리스트를 만드는지 검증
    3. iterator로 for-each 순회가 잘 되는지 검증

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,57 @@
+package lotto.domain;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class LottosTest {
+    @Test
+    void generate로_요청한_개수만큼_로또가_생성된다() {
+        //given
+
+        // when & then
+        assertSimpleTest(() -> {
+            assertAll(
+                    () -> assertEquals(3, Lottos.generate(3).size()),
+                    () -> assertEquals(5, Lottos.generate(5).size()),
+                    () -> assertEquals(10, Lottos.generate(10).size()),
+                    () -> assertEquals(1000, Lottos.generate(1000).size())
+            );
+        });
+    }
+
+    @Test
+    void getLottos로_반환된_리스트는_불변이다() {
+        //given
+        Lottos lottos = Lottos.generate(3);
+
+        //when
+        List<Lotto> lottoList = lottos.getLottos();
+
+        //then
+        assertSimpleTest(() -> {
+            assertThatThrownBy(
+                    () -> lottoList.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)))
+            ).isInstanceOf(UnsupportedOperationException.class);
+        });
+    }
+
+    @Test
+    void iterator로_모든_로또를_순회할_수_있다() {
+        //given
+        Lottos lottos = Lottos.generate(3);
+        int count = 0;
+
+        //when
+        for (Lotto lotto : lottos) {
+            count++;
+        }
+
+        //then
+        assertEquals(3, count);
+    }
+}
