cebe9f25cfafa3268f18dadf16492c67c75384ff
sunJ0120 <sspure1214@gmail.com>
2025-11-02 15:42:10 +0900

    feat: validate에 중복, 범위 체크 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -10,10 +11,32 @@ public class Lotto {
         this.numbers = sort(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
+    private static void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (1 > number || number > 45) {
+                throw new IllegalArgumentException(
+                        String.format("[ERROR] 로또 번호는 1 ~ 45까지의 범위만 허용합니다. : %d", number)
+                );
+            }
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private static void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
     }
 
     private List<Integer> sort(List<Integer> numbers) {
