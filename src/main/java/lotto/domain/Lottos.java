ed6fb028a11864d1fe0bda74f322b3ed007618d1
sunJ0120 <sspure1214@gmail.com>
2025-11-03 22:27:20 +0900

    fix: 매직 넘버 상수로 변경

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.util.LottoConstants.LOTTO_SIZE;
+import static lotto.util.LottoConstants.MAX_LOTTO_NUMBER;
+import static lotto.util.LottoConstants.MIN_LOTTO_NUMBER;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -15,7 +19,7 @@ public class Lottos implements Iterable<Lotto> {
     public static Lottos generate(int count) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
             lottos.add(new Lotto(numbers));
         }
         return new Lottos(lottos);
